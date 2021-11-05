beaedfe5d03270c902bb1ca853745b00ab18ec94
Luis <luis.barbosa@al.infnet.eu.br>
2021-11-05 18:37:47 -0300

    Version 0.01

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -51,27 +51,25 @@
     </div>
   </section>
   <section id="best-sellers">
-    <div class="w-100 vh-100 bg-dark">
+    <div class="w-100 box-container bg-dark">
       <div class="d-flex align-items-center bg-dark title-container">
         <p class="text-light fs-3 ms-5">Best Sellers</p>
       </div>
       <div class="d-flex w-100 bg-light image-container radius-bottom" >
-
-
         <div id="carouselExampleControls" class="w-100 carousel slide carousel-dark" data-bs-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
               <div class="best-product d-flex" style="width: 80%;">
                 <div class="col-md-5 position-relative">
-                  <img src="../assets/products/product_01.jpg" style="object-fit: cover; width: 100%; " alt="">
+                  <img v-bind:src="bestSellers.product1.image" style="object-fit: cover; width: 100%; " alt="">
                   <span class="text-uppercase position-absolute top-0 start-0 bg-info bg-gradient text-light py-1 px-2 mt-4">New</span>
                 </div>
                 <div class="col-md-7 row align-items-center m-5">
-                  <h5 class="fs-4">Roll Top Backpack</h5>
-                  <span class="bg-dark w-25 mx-auto" style="height: 1px;"></span>
-                  <p class="text-muted px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam velit, pariatur molestias, illum ullam quasi vitae exercitationem molestiae, nulla fugiat perspiciatis! Nulla a consequatur unde in velit, ex tempora?</p>
-                  <span class="bg-dark w-25 mx-auto" style="height: 1px;"></span>
-                  <span class="fw-bold fs-5"><span class="fs-6 text-muted text-decoration-line-through fw-normal">$400.00</span>  $365.00</span>
+                  <h5 class="fs-4">{{ bestSellers.product1.title }}</h5>
+                  <span class="hide-component bg-dark w-25 mx-auto" style="height: 1px;"></span>
+                  <p class="hide-component text-muted px-5">{{ bestSellers.product1.text }}</p>
+                  <span class="hide-component bg-dark w-25 mx-auto" style="height: 1px;"></span>
+                  <span class="fw-bold fs-5 my-3"><span class="fs-6 text-muted text-decoration-line-through fw-normal">${{ bestSellers.product1.oldPrice }}</span> ${{ bestSellers.product1.price }}</span>
                   <div class="d-inline mx-auto">
                     <a href="#" class="col-md-3 btn btn-dark me-2">View More</a>
                     <i class="btn btn-outline-dark col-md-1 bi bi-heart"></i>
@@ -83,15 +81,15 @@
             <div class="carousel-item">
               <div class="best-product d-flex" style="width: 80%;">
                 <div class="col-md-5 position-relative">
-                  <img src="../assets/products/product_02.jpg" style="object-fit: cover; width: 100%; " alt="">
+                  <img v-bind:src="bestSellers.product2.image" style="object-fit: cover; width: 100%; " alt="">
                   <span class="text-uppercase position-absolute top-0 start-0 bg-info bg-gradient text-light py-1 px-2 mt-4">New</span>
                 </div>
                 <div class="col-md-7 row align-items-center m-5">
-                  <h5 class="fs-4">Bomber in Cottone</h5>
-                  <span class="bg-dark w-25 mx-auto" style="height: 1px;"></span>
-                  <p class="text-muted px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam velit, pariatur molestias, illum ullam quasi vitae exercitationem molestiae, nulla fugiat perspiciatis! Nulla a consequatur unde in velit, ex tempora?</p>
-                  <span class="bg-dark w-25 mx-auto" style="height: 1px;"></span>
-                  <span class="fw-bold fs-5"><span class="fs-6 text-muted text-decoration-line-through fw-normal">$260.00</span>  $290.00</span>
+                  <h5 class="fs-4">{{ bestSellers.product2.title }}</h5>
+                  <span class="hide-component bg-dark w-25 mx-auto" style="height: 1px;"></span>
+                  <p class="hide-component text-muted px-5">{{ bestSellers.product2.text }}</p>
+                  <span class="hide-component bg-dark w-25 mx-auto" style="height: 1px;"></span>
+                  <span class="fw-bold fs-5 my-3"><span class="fs-6 text-muted text-decoration-line-through fw-normal">${{ bestSellers.product2.oldPrice }}</span>  ${{ bestSellers.product2.price }}</span>
                   <div class="d-inline mx-auto">
                     <a href="#" class="col-md-3 btn btn-dark me-2">View More</a>
                     <i class="btn btn-outline-dark col-md-1 bi bi-heart"></i>
@@ -103,15 +101,15 @@
             <div class="carousel-item">
               <div class="best-product d-flex" style="width: 80%;">
                 <div class="col-md-5 position-relative">
-                  <img src="../assets/products/product_03.jpg" style="object-fit: cover; width: 100%; " alt="">
+                  <img v-bind:src="bestSellers.product3.image" style="object-fit: cover; width: 100%; " alt="">
                   <span class="text-uppercase position-absolute top-0 start-0 bg-info bg-gradient text-light py-1 px-2 mt-4">New</span>
                 </div>
                 <div class="col-md-7 row align-items-center m-5">
-                  <h5 class="fs-4">Hooded Coat</h5>
-                  <span class="bg-dark w-25 mx-auto" style="height: 1px;"></span>
-                  <p class="text-muted px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam velit, pariatur molestias, illum ullam quasi vitae exercitationem molestiae, nulla fugiat perspiciatis! Nulla a consequatur unde in velit, ex tempora?</p>
-                  <span class="bg-dark w-25 mx-auto" style="height: 1px;"></span>
-                  <span class="fw-bold fs-5"><span class="fs-6 text-muted text-decoration-line-through fw-normal">$260.00</span>  $290.00</span>
+                  <h5 class="fs-4">{{ bestSellers.product3.title }}</h5>
+                  <span class="hide-component bg-dark w-25 mx-auto" style="height: 1px;"></span>
+                  <p class="hide-component text-muted px-5">{{ bestSellers.product3.text }}</p>
+                  <span class="hide-component bg-dark w-25 mx-auto" style="height: 1px;"></span>
+                  <span class="fw-bold fs-5 my-3"><span class="fs-6 text-muted text-decoration-line-through fw-normal">${{ bestSellers.product3.oldPrice }}</span> ${{ bestSellers.product3.price }}</span>
                   <div class="d-inline mx-auto">
                     <a href="#" class="col-md-3 btn btn-dark me-2">View More</a>
                     <i class="btn btn-outline-dark col-md-1 bi bi-heart"></i>
@@ -129,9 +127,8 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
-        </div>      
+        </div>
       </div>
-
     </div>
   </section>
 
@@ -161,7 +158,31 @@ export default {
             text: "ss '22 Collection",
             btnText1: "Shop Now"
           }
-        }
+        },
+        bestSellers: 
+          {
+            product1: {
+              title: "Roll Top Backpack",
+              image: require("../assets/products/product_01.jpg"), 
+              text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, nulla. Aspernatur possimus provident mollitia perferendis veritatis perspiciatis repellat, necessitatibus, amet inventore illo blanditiis aperiam tempora soluta dolorum eum accusamus sunt.",
+              price: 365.00,
+              oldPrice: 400.00
+            },
+            product2: {
+              title: "Bomber in Cotton",
+              image: require("../assets/products/product_02.jpg"), 
+              text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque a dignissimos, nesciunt consequatur omnis, illo modi unde officia, quisquam corporis incidunt voluptates quidem autem repudiandae? Temporibus natus harum reiciendis quas.",
+              price: 290.00,
+              oldPrice: 260.00
+            },
+            product3: {
+              title: "Hooded Coat",
+              image: require("../assets/products/product_03.jpg"), 
+              text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti veniam nesciunt temporibus eveniet aspernatur! Tenetur molestias esse accusamus unde quidem, non harum reprehenderit quasi hic exercitationem, aspernatur nobis vero!",
+              price: 320.00,
+              oldPrice: 295.00
+            },
+          }
     }
   }
 }
@@ -176,20 +197,22 @@ export default {
   top: 50%;
   left: 30%;
 }
+.box-container {
+  min-height: 100vh;
+}
+
 .title-container {
-  height: 15%;
+  min-height: 15vh;
 }
 .image-container {
-  min-height: 85%;
+  min-height: 85vh;
 }
 .radius-bottom {
   border-radius: 0 0 50px 50px;
 }
-
 .best-product {
   margin: 0 auto;
 }
-
 .btn-dark:hover {
   background-color: transparent;
   color: black;
@@ -200,5 +223,9 @@ export default {
     display: flex;
     flex-direction: column;
   }
+  .hide-component  {
+    display: none;
+  }
+ 
 }
 </style>
