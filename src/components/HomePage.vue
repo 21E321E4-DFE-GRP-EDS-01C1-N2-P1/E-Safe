6ec81f50bfc04b459ff5076a855b5e8874528c4d
Luis <luis.barbosa@al.infnet.eu.br>
2021-11-06 23:01:30 -0300

    Version 0.01

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -239,8 +239,48 @@
       </div>
     </div>
   </section>
-  
-  
+  <section id="footer" class="bg-dark pt-5">
+    <div class="container">
+      <footer class="py-3">
+        <div class="row">
+          <div class="col-md-2">
+            <h5 class="text-light">Section</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Woman</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Men</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Outlet</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-2">
+            <h5 class="text-light">Institutional</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contact Us</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy Policy</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">My Account</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+            </ul>
+          </div>
+          
+          <div class="col-md-8">
+            <img src="https://cdn-icons-png.flaticon.com/512/217/217422.png" class="mx-4 resize-icon" width="100" alt="paypal">
+            <img src="https://cdn-icons-png.flaticon.com/512/1488/1488176.png" class="mx-4 resize-icon" width="100" alt="money back">
+            <img src="https://cdn-icons.flaticon.com/png/512/3009/premium/3009916.png?token=exp=1636249784~hmac=8511a89887cf0f537d6885fa721106b0" class="mx-4 resize-icon" width="100" alt="credit card">
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-between py-4 my-4 border-top text-light">
+          <p>© 2021 Coding Style. All rights reserved.</p>
+          <ul class="list-unstyled d-flex">
+            <li class="ms-3"><a href="#"><i class="bi bi-twitter fs-4 text-light"></i></a></li>
+            <li class="ms-3"><a href="#"><i class="bi bi-facebook fs-4 text-light"></i></a></li>
+            <li class="ms-3"><a href="#"><i class="bi bi-instagram fs-4 text-light"></i></a></li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  </section>
 
 
 </template>
@@ -417,7 +457,7 @@ export default {
   z-index: 100;
 }
 
-@media (max-width: 960px) {
+@media (max-width: 760px) {
   .best-product {
     display: flex;
     flex-direction: column;
@@ -429,6 +469,17 @@ export default {
   .container {
     flex-direction: column;
     margin-bottom: 20px;
+  }
+}
+
+@media (max-width: 986px) {
+  .container {
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+
+  .resize-icon {
+    transform: scale(0.8)
   }
  
 }
